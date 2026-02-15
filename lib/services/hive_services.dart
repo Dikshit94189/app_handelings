add4893c1513343ecffd9b0d913baa101b13fb69
Dikshit Sharma <noreply@feedback.github.com>
2026-02-15 09:47:39 +0530

    sperate

---
+++ lib/services/hive_services.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class HiveScrvice{
+  static const String _boxName = 'appBox';
+  static Box? _box;
+
+  static Future<void> init() async{
+    await Hive.initFlutter();
+    _box = await Hive.openBox(_boxName);
+  }
+
+  static Box get box{
+    if(_box == null){
+      throw Exception("Hive box not initialized. Call HiveService.init()");
+    }
+    return _box!;
+  }
+
+  static bool isFirstTime(){
+    return box.get('isFirstTime' , defaultValue: true);
+  }
+
+  static Future<void> setFirstTime(bool value) async{
+    await box.put('isFirstTime', value);
+  }
+
+  static Future<void> clearAll() async{
+    await box.clear();
+  }
+
+
+}
