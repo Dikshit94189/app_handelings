f96705e13ffb96b5b1ada78b071b897f0362fef1
Dikshit Sharma <noreply@feedback.github.com>
2026-02-23 22:25:59 +0530

    issue found

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../utils/api_response.dart';
+import '../view_model/quote_view.dart';
 import 'home_screen.dart';
 
 // class MainScreen extends StatefulWidget {
@@ -55,7 +60,7 @@ class _MainScreenState extends State<MainScreen> {
 
   final List<Widget> _screens = const [
     HomeScreen(),
-    Center(child: Text("Profile Screen")),
+    ProfileScreen()
   ];
 
   @override
@@ -84,6 +89,73 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final viewModel = context.read<QuoteViewModel>();
+
+      viewModel.getRandomImages();   // 🔥 YOU MISSED THIS
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(children: [
+        Consumer<QuoteViewModel>(
+          builder: (context , viewModel , child){
+            switch(viewModel.imagesResponse.status){
+              case Status.loading:
+                return CupertinoActivityIndicator();
+              case Status.complete:
+                final images = viewModel.imagesResponse.data;
+
+                return SizedBox(
+                  height: 250,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: images!.length,
+                    itemBuilder: (context, index) {
+                      final image = images[index];
+
+                      return Container(
+                        width: 250,
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: CachedNetworkImage(
+                          imageUrl: image.url ?? "",
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  ),
+                );
+              case Status.error:
+                return Text("Error");
+
+            }
+
+          },
+        ),
+      ],)),
     );
   }
 }
