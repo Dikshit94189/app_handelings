5a14dae581f97e0bb4b4879dff427c56f8352e54
Dikshit Sharma <noreply@feedback.github.com>
2026-02-25 22:07:16 +0530

    add_border_images

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:new_tasks/screens/profile_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../utils/api_response.dart';
@@ -93,76 +94,3 @@ class _MainScreenState extends State<MainScreen> {
   }
 }
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
-
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final viewModel = context.read<QuoteViewModel>();
-
-      viewModel.getRandomImages();   // 🔥 YOU MISSED THIS
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Column(children: [
-        Consumer<QuoteViewModel>(
-          builder: (context , viewModel , child){
-            switch(viewModel.imagesResponse.status){
-              case Status.loading:
-                return CupertinoActivityIndicator();
-              case Status.complete:
-                final images = viewModel.imagesResponse.data;
-
-                return SizedBox(
-                  height: 250,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: images!.length,
-                    itemBuilder: (context, index) {
-                      final image = images[index];
-
-                      return Container(
-                        width: 250,
-                        margin: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: CachedNetworkImage(
-                            imageUrl: image.downloadUrl,
-                            fit: BoxFit.cover,
-                            placeholder: (context, url) =>
-                            const CupertinoActivityIndicator(),
-                            errorWidget: (context, url, error) =>
-                            const Icon(Icons.broken_image),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                );
-              case Status.error:
-                return Text("Error");
-
-            }
-
-          },
-        ),
-      ],)),
-    );
-  }
-}
