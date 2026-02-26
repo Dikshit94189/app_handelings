8c972bcad0ade54cff3cbb8fd8127e5f09721d1a
Dikshit Sharma <noreply@feedback.github.com>
2026-02-26 21:02:18 +0530

    profile_screen_view

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -8,46 +8,6 @@ import '../utils/api_response.dart';
 import '../view_model/quote_view.dart';
 import 'home_screen.dart';
 
-// class MainScreen extends StatefulWidget {
-//   const MainScreen({super.key});
-//
-//   @override
-//   State<MainScreen> createState() => _MainScreenState();
-// }
-//
-// class _MainScreenState extends State<MainScreen> {
-//   int _currentIndex = 0;
-//
-//   final List<Widget> _screens = [
-//     HomeScreen(),
-//     HomeScreen(),
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: _screens[_currentIndex],
-//       bottomNavigationBar: BottomNavigationBar(
-//         currentIndex: _currentIndex,
-//         onTap: (index) {
-//           setState(() {
-//             _currentIndex = index;
-//           });
-//         },
-//         items: const [
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: "Home",
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.dashboard),
-//             label: "Profile",
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
